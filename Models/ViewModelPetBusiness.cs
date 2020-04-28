979bb6a17637863f0b875cd7dcd2d2f6b0b2f9af
christykyang@gmail.com <christykyang@gmail.com>
2020-04-28 12:28:58 -0500

    Changed ViewModel for SearchPetBusinesses. TEST3

--- Models/ViewModelPetBusiness.cs
+++ Models/ViewModelPetBusiness.cs
@@ -14,19 +14,28 @@ namespace PawentsOneStopShop.Models
         [Display(Name = "Address")]
         public Address Address { get; set; }
         public int? AddressId { get; set; }
+        public List<Address> Addresses { get; set; }
         [Display(Name = "Identity User")]
         public string IdentityUserId { get; set; }
         public IdentityUser IdentityUser { get; set; }
         [Display(Name = "Type of Business")]
         public int? BusinessTypeId { get; set; }
         public BusinessType BusinessType { get; set; }
+        public List<BusinessType> BusinessTypes { get; set; }
+        [Display(Name = "Pet Business")]
         public int PetBusinessId { get; set; }
         public PetBusiness PetBusiness { get; set; }
+        public List<PetBusiness> PetBusinesses { get; set; }
         public Follow Follow { get; set; }
         public int? FollowId { get; set; }
         [Display(Name = "Follow Status")]
         public bool IsFollowing { get; set; }
         public int PetOwnerId { get; set; }
         public PetOwner PetOwner { get; set; }
+        [Display(Name = "Service")]
+        public int? ServiceId { get; set; }
+        public Service Service { get; set; }
+        public List<Service> Services { get; set }
+
     }
 }
