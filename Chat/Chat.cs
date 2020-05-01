a9cda9f307fba03259591f9909ae3023de13855f
christykyang@gmail.com <christykyang@gmail.com>
2020-05-01 04:51:47 -0500

    added signalR method

---
+++ Chat/Chat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using PawentsOneStopShop.Contracts;
+
+namespace PawentsOneStopShop.Chat
+{
+    public class Chat : Hub
+    {
+        private readonly IRepositoryWrapper _repo;
+        public Chat(IRepositoryWrapper repo)
+        {
+            _repo = repo;
+        }
+        public async Task SendMessage(string userFrom, string userTo, string message, string timeStamp)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", userFrom, userTo, message, timeStamp);
+        }
+
+
+    }
+}
