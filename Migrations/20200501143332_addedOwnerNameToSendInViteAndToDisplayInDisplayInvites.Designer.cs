f5d9c2135cb255e44e9b2ef39469c819bf6b4697
christykyang@gmail.com <christykyang@gmail.com>
2020-05-01 09:34:18 -0500

    updated databases

--- Migrations/20200501143332_addedOwnerNameToSendInViteAndToDisplayInDisplayInvites.Designer.cs
+++ Migrations/20200501143332_addedOwnerNameToSendInViteAndToDisplayInDisplayInvites.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawentsOneStopShop.Data;
 
 namespace PawentsOneStopShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200501143332_addedOwnerNameToSendInViteAndToDisplayInDisplayInvites")]
+    partial class addedOwnerNameToSendInViteAndToDisplayInDisplayInvites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
